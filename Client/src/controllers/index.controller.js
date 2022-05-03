065f30f41820e12889f6e84444136240764d9b7d
Rubén Alonso Hermosilla Leyton <ruben.hermosilla@mail.udp.cl>
2022-05-03 14:45:28 -0400

    Redis algo

--- Client/src/controllers/index.controller.js
+++ Client/src/controllers/index.controller.js
@@ -1,5 +1,19 @@
 const grpc = require("@grpc/grpc-js");
 const PROTO_PATH = "./src/controllers/searchInventory.proto"
+var redis = require('redis')
+
+const redis_client = redis.createClient({
+    port: 6379,
+    host: "172.20.0.3",
+});
+
+redis_client.on('ready',()=>{
+    console.log("Redis listo")
+})
+
+redis_client.connect();
+
+console.log('Redis conection: '+redis_client.isOpen);
 
 var protoLoader = require("@grpc/proto-loader");
 
@@ -22,17 +36,32 @@ const client = new InventorySearch(
 
 const searchitems=async (req,res)=>{
     const busqueda=req.params.name
-    client.GetServerResponse({message:busqueda}, (error,items) =>{
-        if(error){
-            console.log("error aaaa")
-            res.status(400).json(error);
+    let cache = null;
+    async () =>{
+        let reply = await redis_client.get(busqueda);
+
+        if(reply){
+            cache = JSON.parse(reply);
+            console.log("Esta en cache :D")
+            res.status(200).json(cache)
         }
         else{
-            res.status(200).json(items);
-
-        }
+            console.log("No esta en cache D:")
+            client.GetServerResponse({message:busqueda}, (error,items) =>{
+                if(error){
+                    console.log("error aaaa")
+                    res.status(400).json(error);
+                    redis_client.set(busqueda,items)
+                }
+                else{
+                    res.status(200).json(items);
         
-    });
+                }
+            });
+        }
+
+    }
+    
 }
 
 
